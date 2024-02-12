c97a88393306ecd197c7b0530c3398215e680f1d
Ain Ghazal <ainghazal42@gmail.com>
2024-02-12 23:27:16 +0100

    test service

--- internal/networkio/networkio_test.go
+++ internal/networkio/networkio_test.go
@@ -3,7 +3,6 @@ package networkio
 import (
 	"bytes"
 	"context"
-	"errors"
 	"net"
 	"testing"
 
@@ -11,57 +10,8 @@ import (
 	"github.com/ooni/minivpn/internal/vpntest"
 )
 
-type mockedConn struct {
-	conn    *vpntest.Conn
-	dataIn  [][]byte
-	dataOut [][]byte
-}
-
-func (mc *mockedConn) DataIn() [][]byte {
-	return mc.dataIn
-}
-
-func newDialer(underlying *mockedConn) *vpntest.Dialer {
-	dialer := &vpntest.Dialer{
-		MockDialContext: func(ctx context.Context, network, address string) (net.Conn, error) {
-			return underlying.conn, nil
-		},
-	}
-	return dialer
-}
-
-func newMockedConn(network string, dataIn, dataOut [][]byte) *mockedConn {
-	conn := &mockedConn{
-		dataIn:  dataIn,
-		dataOut: dataOut,
-	}
-	conn.conn = &vpntest.Conn{
-		MockLocalAddr: func() net.Addr {
-			addr := &vpntest.Addr{
-				MockString:  func() string { return "1.2.3.4" },
-				MockNetwork: func() string { return network },
-			}
-			return addr
-		},
-		MockRead: func(b []byte) (int, error) {
-			if len(conn.dataOut) > 0 {
-				copy(b[:], conn.dataOut[0])
-				ln := len(conn.dataOut[0])
-				conn.dataOut = conn.dataOut[1:]
-				return ln, nil
-			}
-			return 0, errors.New("EOF")
-		},
-		MockWrite: func(b []byte) (int, error) {
-			conn.dataIn = append(conn.dataIn, b)
-			return len(b), nil
-		},
-	}
-	return conn
-}
-
 func Test_TCPLikeConn(t *testing.T) {
-	t.Run("A tcp-like conn implements the openvpn framing", func(t *testing.T) {
+	t.Run("A tcp-like conn implements the openvpn size framing", func(t *testing.T) {
 		dataIn := make([][]byte, 0)
 		dataOut := make([][]byte, 0)
 		// write size
@@ -88,7 +38,7 @@ func Test_TCPLikeConn(t *testing.T) {
 
 		written := []byte("ingirumimusnocteetconsumimurigni")
 		framingConn.WriteRawPacket(written)
-		gotWritten := underlying.DataIn()
+		gotWritten := underlying.NetworkWrites()
 		if !bytes.Equal(gotWritten[0], append([]byte{0, byte(len(written))}, written...)) {
 			t.Errorf("got = %v, want = %v", gotWritten, written)
 		}
@@ -106,7 +56,7 @@ func Test_UDPLikeConn(t *testing.T) {
 		underlying := newMockedConn("udp", dataIn, dataOut)
 		testDialer := newDialer(underlying)
 		dialer := NewDialer(log.Log, testDialer)
-		framingConn, err := dialer.DialContext(context.Background(), "tcp", "1.1.1.1")
+		framingConn, err := dialer.DialContext(context.Background(), "udp", "1.1.1.1")
 		if err != nil {
 			t.Errorf("should not error getting a framingConn")
 		}
@@ -119,7 +69,7 @@ func Test_UDPLikeConn(t *testing.T) {
 		}
 		written := []byte("ingirumimusnocteetconsumimurigni")
 		framingConn.WriteRawPacket(written)
-		gotWritten := underlying.DataIn()
+		gotWritten := underlying.NetworkWrites()
 		if !bytes.Equal(gotWritten[0], written) {
 			t.Errorf("got = %v, want = %v", gotWritten, written)
 		}
